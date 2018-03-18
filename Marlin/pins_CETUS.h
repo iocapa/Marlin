3a2432f6bb083f08fc3825e53eb555276eb93181
Pavel Ionut Catalin <pavel.ionut.catalin.88@gmail.com>
2018-03-18 19:24:37 +0200

    Removed tabs

--- Marlin/pins_CETUS.h
+++ Marlin/pins_CETUS.h
@@ -47,82 +47,82 @@
 //
 // Schematic pin mapping
 //
-#define CETUS_T_HEND			24 //TEMP_0_PIN
-#define CETUS_T_BED				25 //TEMP_BED_PIN
-#define CETUS_DOOR_CHECK		26
-#define CETUS_SD_POWER			27
-#define CETUS_Z_LIMIT			28 //Z_MIN_PIN
-#define CETUS_HEAD_AUX			29 //Z_MIN_PROBE_PIN, modifications required, check schematic.
-#define CETUS_X_LIMIT			30 //X_MIN_PIN
-#define CETUS_Y_LIMIT			31 //Y_MIN_PIN
-#define CETUS_MCU_POW_ON		0  //PS_ON_PIN
-#define CETUS_MCU_PANEL_LED		1  //LED_PIN -> ??
-#define CETUS_AUX_FLS			2
-#define CETUS_EXT_SW			3  //KILL_PIN -> ??
-#define CETUS_SPI_SS			4  //SDSS
-#define CETUS_SPI_MOSI			5
-#define CETUS_SPI_MISO			6
-#define CETUS_SPI_SCK			7
-#define CETUS_E_STEP			16  //E_STEP_PIN
-#define CETUS_E_DIR				17  //E_DIR_PIN
-#define CETUS_X_DIR				18  //X_DIR_PIN
-#define CETUS_X_STEP			19  //X_STEP_PIN
-#define CETUS_Y_DIR				20  //Y_DIR_PIN
-#define CETUS_Y_STEP			21  //Y_STEP_PIN
-#define CETUS_Z_DIR				22  //Z_DIR_PIN
-#define CETUS_Z_STEP			23  //Z_STEP_PIN
-#define CETUS_USB_TX			8
-#define CETUS_USB_RX			9
-#define CETUS_AUX_RXD			10
-#define CETUS_AUX_TXD			11
-#define CETUS_MCU_LIGTH			12  //FAN_PIN, modifications required, check schematic.
-#define CETUS_MCU_HEATER_BED	13  //HEATER_BED_PIN
-#define CETUS_MCU_HEATER		14  //HEATER_0_PIN
-#define CETUS_MCU_BUZZER		15  //BEEPER_PIN -> ??
+#define CETUS_T_HEND                24 //TEMP_0_PIN
+#define CETUS_T_BED                 25 //TEMP_BED_PIN
+#define CETUS_DOOR_CHECK            26
+#define CETUS_SD_POWER              27
+#define CETUS_Z_LIMIT               28 //Z_MIN_PIN
+#define CETUS_HEAD_AUX              29 //Z_MIN_PROBE_PIN, modifications required, check schematic.
+#define CETUS_X_LIMIT               30 //X_MIN_PIN
+#define CETUS_Y_LIMIT               31 //Y_MIN_PIN
+#define CETUS_MCU_POW_ON            0  //PS_ON_PIN
+#define CETUS_MCU_PANEL_LED         1  //LED_PIN -> ??
+#define CETUS_AUX_FLS               2
+#define CETUS_EXT_SW                3  //KILL_PIN -> ??
+#define CETUS_SPI_SS                4  //SDSS
+#define CETUS_SPI_MOSI              5
+#define CETUS_SPI_MISO              6
+#define CETUS_SPI_SCK               7
+#define CETUS_E_STEP                16  //E_STEP_PIN
+#define CETUS_E_DIR                 17  //E_DIR_PIN
+#define CETUS_X_DIR                 18  //X_DIR_PIN
+#define CETUS_X_STEP                19  //X_STEP_PIN
+#define CETUS_Y_DIR                 20  //Y_DIR_PIN
+#define CETUS_Y_STEP                21  //Y_STEP_PIN
+#define CETUS_Z_DIR                 22  //Z_DIR_PIN
+#define CETUS_Z_STEP                23  //Z_STEP_PIN
+#define CETUS_USB_TX                8
+#define CETUS_USB_RX                9
+#define CETUS_AUX_RXD               10
+#define CETUS_AUX_TXD               11
+#define CETUS_MCU_LIGTH             12  //FAN_PIN, modifications required, check schematic.
+#define CETUS_MCU_HEATER_BED        13  //HEATER_BED_PIN
+#define CETUS_MCU_HEATER            14  //HEATER_0_PIN
+#define CETUS_MCU_BUZZER            15  //BEEPER_PIN -> ??
 
 //
 // Steppers and limit switches
 //
-#define X_STEP_PIN         		CETUS_X_STEP
-#define Y_STEP_PIN         		CETUS_Y_STEP
-#define Z_STEP_PIN         		CETUS_Z_STEP
-#define E_STEP_PIN         		CETUS_E_STEP
+#define X_STEP_PIN                  CETUS_X_STEP
+#define Y_STEP_PIN                  CETUS_Y_STEP
+#define Z_STEP_PIN                  CETUS_Z_STEP
+#define E_STEP_PIN                  CETUS_E_STEP
 
-#define X_DIR_PIN          		CETUS_X_DIR
-#define Y_DIR_PIN          		CETUS_Y_DIR
-#define Z_DIR_PIN          		CETUS_Z_DIR
-#define E_DIR_PIN          		CETUS_E_DIR
+#define X_DIR_PIN                   CETUS_X_DIR
+#define Y_DIR_PIN                   CETUS_Y_DIR
+#define Z_DIR_PIN                   CETUS_Z_DIR
+#define E_DIR_PIN                   CETUS_E_DIR
 
-#define X_MIN_PIN				CETUS_X_LIMIT
-#define Y_MIN_PIN				CETUS_Y_LIMIT
-#define Z_MIN_PIN				CETUS_Z_LIMIT
+#define X_MIN_PIN                   CETUS_X_LIMIT
+#define Y_MIN_PIN                   CETUS_Y_LIMIT
+#define Z_MIN_PIN                   CETUS_Z_LIMIT
 
 #ifdef CETUS_PROBE_ON_AUX
-#define Z_MIN_PROBE_PIN			CETUS_HEAD_AUX
+#define Z_MIN_PROBE_PIN             CETUS_HEAD_AUX
 #else
-#define Z_MIN_PROBE_PIN			-1
+#define Z_MIN_PROBE_PIN            -1
 #
 
 //
 // Heaters and temperature
 //
-#define TEMP_0_PIN 				CETUS_T_HEND
-#define TEMP_BED_PIN			CETUS_T_BED
-#define HEATER_0_PIN			CETUS_MCU_HEATER
-#define HEATER_BED_PIN			CETUS_MCU_HEATER_BED
+#define TEMP_0_PIN                  CETUS_T_HEND
+#define TEMP_BED_PIN                CETUS_T_BED
+#define HEATER_0_PIN                CETUS_MCU_HEATER
+#define HEATER_BED_PIN              CETUS_MCU_HEATER_BED
 
 //
 // Misc
 //
-#define PS_ON_PIN				CETUS_MCU_POW_ON
-#define BEEPER_PIN				CETUS_MCU_BUZZER
-#define SDSS					CETUS_SPI_SS
-#define LED_PIN					CETUS_MCU_PANEL_LED
-#define KILL_PIN				CETUS_EXT_SW
+#define PS_ON_PIN                CETUS_MCU_POW_ON
+#define BEEPER_PIN                CETUS_MCU_BUZZER
+#define SDSS                    CETUS_SPI_SS
+#define LED_PIN                    CETUS_MCU_PANEL_LED
+#define KILL_PIN                CETUS_EXT_SW
 
 #ifdef CETUS_FAN_ON_LIGTH_PIN
-#define FAN_PIN					CETUS_MCU_LIGTH
+#define FAN_PIN                    CETUS_MCU_LIGTH
 #else
-#define FAN_PIN 				-1
+#define FAN_PIN                 -1
 #endif
 
